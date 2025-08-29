d23b7bee06cc3e37467a737ef67ce727fff1768d
Christian Penticostes <137411617+Christian-Penticostes@users.noreply.github.com>
2025-08-29 17:23:55 +0800

    Updated codes with new features/fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,37 +15,37 @@ import download from '../assets/downloads-w.png';
 const Hero = () => {
   return (
     <div className="flex justify-center items-center lg:h-[calc(98dvh-10vh)] max-w-[1550px] mx-auto px-7 max-lg:mb-[30px]">
-        <div className='flex gap-[55px] max-lg:flex-col max-md:gap-[55px] max-sm:mb-[30px]'>
-            <div className='flex justify-center flex-col gap-4'>
-                <img src={facebook} alt="Facebook" className="dark:hidden w-7 h-7 hover:scale-110 transition" />
+        <div className='flex gap-[55px] max-lg:flex-col max-lg:gap-[35px] max-md:gap-[35px] max-sm:mb-[30px]'>
+            <div className='flex justify-center flex-col gap-5 max-lg:items-center max-lg:flex-row max-lg:order-2 max-md:order-2'>
+                <img src={facebook} alt="Facebook" className="dark:hidden w-7 h-7 hover:scale-110 transition cursor-pointer" />
                 <img src={facebookW} alt="Facebook" className="hidden dark:block w-7 h-7 hover:scale-110 transition" />
 
-                <img src={linkedin} alt="LinkedIn" className="dark:hidden w-7 h-7 hover:scale-110 transition" />
+                <img src={linkedin} alt="LinkedIn" className="dark:hidden w-7 h-7 hover:scale-110 transition cursor-pointer" />
                 <img src={linkedinW} alt="LinkedIn White" className="hidden dark:block w-7 h-7 hover:scale-110 transition" />
 
-                <img src={github} alt="GitHub" className="dark:hidden w-7 h-7 hover:scale-110 transition" />
+                <img src={github} alt="GitHub" className="dark:hidden w-7 h-7 hover:scale-110 transition cursor-pointer" />
                 <img src={githubW} alt="GitHub White" className="hidden dark:block w-7 h-7 hover:scale-110 transition" />       
 
-                <img src={email} alt="Email" className="dark:hidden w-7 h-7 hover:scale-110 transition" />
+                <img src={email} alt="Email" className="dark:hidden w-7 h-7 hover:scale-110 transition cursor-pointer" />
                 <img src={emailW} alt="Email White" className="hidden dark:block w-7 h-7 hover:scale-110 transition" />
             </div>
-            <div className='flex items-center max-lg:justify-center max-lg:mt-[80px] max-md:mt-[50px]'>
+            <div className='flex items-center max-lg:justify-center max-lg:mt-[80px] max-md:mt-[50px] max-lg:order-1 max-md:order-1'>
                 <img src={myPhoto} alt="Christian" width={380} height={380} className="rounded-full object-cover"/>
             </div>
-            <div className='flex justify-center flex-col max-lg:items-center'>
+            <div className='flex justify-center flex-col max-lg:items-center max-lg:order-3 max-md:order-3'>
                 <div className='flex items-center gap-[6px] mb-[15px] max-md:mb-[5px]'>
-                    <p className="dark:text-[#fafafa]">Hi, Great to See You Here!</p>
+                    <p className="dark:text-[#fafafa] text-sm" style={{ fontFamily: '"Poppins"' }}>Hi, Great to See You Here!</p>
                 </div>
                 <div className='items-center gap-[6px] mb-[15px] max-md:mb-[5px]'>
-                    <h1 className="animated-gradient-text text-4xl font-extrabold leading-tight max-lg:text-center" style={{ fontFamily: '"Roboto Flex"' }}>I'm Christian R. Penticostes</h1>
-                    <h1 className="animated-gradient-text text-4xl font-extrabold leading-tight max-lg:text-center" style={{ fontFamily: '"Roboto Flex"' }}>An Aspiring Web & Software Developer</h1>
+                    <h1 className="animated-gradient-text text-4xl font-extrabold leading-tight max-lg:text-center max-md:text-3xl max-md:mt-2" style={{ fontFamily: '"Roboto Flex"' }}>I'm Christian R. Penticostes</h1>
+                    <h1 className="animated-gradient-text text-4xl font-extrabold leading-tight max-lg:text-center max-md:text-3xl max-md:mb-2 max-sm:text-xl" style={{ fontFamily: '"Roboto Flex"' }}>An Aspiring Web & Software Developer</h1>
                 </div>
                 <div className='flex gap-[100px] mb-[12px]'>
-                    <p className='dark:text-[#fafafa] text-md w-[40vw] leading-[26px] mb-[30px] max-lg:w-[80vw] max-lg:text-center'>I’m passionate Full-Stack Developer with strong foundation in back-end development. Experienced in building dynamic and interactive web applications that enhance user experience. Eager to expand my skills and contribute to real-world projects.</p>
+                    <p className='dark:text-[#fafafa] text-sm w-[40vw] leading-[26px] mb-[30px] max-lg:w-[80vw] max-lg:text-center' style={{ fontFamily: '"Poppins"' }}>I’m passionate Full-Stack Developer with strong foundation in back-end development. Experienced in building dynamic and interactive web applications that enhance user experience. Eager to expand my skills and contribute to real-world projects.</p>
                 </div>
                 <div className='flex gap-[18px]'>
-                    <button className="bg-black dark:bg-[#fafafa] dark:text-[#343434] text-white font-semibold px-6 py-3 rounded-lg shadow-md hover:bg-gray-200 transition">View Resume</button>
-                    <button className="bg-black dark:bg-[#fafafa] p-3 rounded-lg shadow-md hover:bg-gray-200 transition"><img src={download} alt="Download" className="w-6 h-6 dark:hidden" /><img src={downloadW} alt="Download" className="hidden dark:block w-6 h-6" /></button>
+                    <button className="bg-black dark:bg-[#fafafa] dark:text-[#343434] text-white font-semibold px-6 py-3 rounded-lg shadow-md hover:bg-gray-800 dark:hover:bg-gray-300 transition ">View Resume</button>
+                    <button className="bg-black dark:bg-[#fafafa] p-3 rounded-lg shadow-md hover:bg-gray-800 dark:hover:bg-gray-300 transition"><img src={download} alt="Download" className="w-6 h-6 dark:hidden" /><img src={downloadW} alt="Download" className="hidden dark:block w-6 h-6" /></button>
                 </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-20 max-w-[1440px] mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl text-[#4169E1] m-0 leading-none' style={{ fontFamily: '"Alfa Slab One"' }}>「Christian」</h1>
+        <h1 className='w-full text-3xl text-[#4169E1] m-0 leading-none animated-gradient-text' style={{ fontFamily: '"Alfa Slab One"' }}>「Christian」</h1>
         <ul className="hidden md:flex items-center uppercase mr-2 dark:text-[#fafafa]">
             <li className="p-4 text-md font-light text-[#343434] dark:text-[#fafafa]" style={{ fontFamily: '"Poppins"' }}>Home</li>
             <li className="p-4 text-md font-light text-[#343434] dark:text-[#fafafa]" style={{ fontFamily: '"Poppins"' }}>About</li>
@@ -42,7 +42,7 @@ const Navbar = () => {
         <div onClick={handleNav} className='block md:hidden mr-4 cursor-pointer text-[#343434] dark:text-[#fafafa]'>
             {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
         </div>
-        <div className={`fixed top-0 h-full w-[60%] shadow-[4px_0_15px_rgba(0,0,0,0.3)] dark:shadow-[4px_0_15px_rgba(255,255,255,0.3)]  backdrop-blur-xl transition-all duration-500 ease-in-out md:hidden ${nav ? 'left-0' : 'left-[-100%]'}`}>
+        <div className={`fixed top-0 h-full w-[60%] shadow-[4px_0_15px_rgba(0,0,0,0.3)] dark:shadow-[4px_0_15px_rgba(255,255,255,0.3)]  backdrop-blur-3xl backdrop-brightness-150 transition-all duration-500 ease-in-out md:hidden ${nav ? 'left-0' : 'left-[-100%]'}`}>
             <h1 className='w-full text-3xl text-[#4169E1] mt-10 mb-4 text-center leading-none' style={{ fontFamily: '"Alfa Slab One"' }}>「Christian」</h1>
             <ul className='uppercase p-4'>
                 <li className='p-4 text-md font-light border-b border-gray-600 text-[#343434] dark:text-[#fafafa] dark:border-gray-300'>Home</li>
